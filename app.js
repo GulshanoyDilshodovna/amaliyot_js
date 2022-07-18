2704552b75cc7afb701382a19d09e8c11a0304b9
Gulshanoy <gulshanoydilshodovna@gmail.com>
2022-07-18 23:09:36 +0500

    Add background

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
-let ism = prompt("Ismingizni kiriting: ")
-let result = ism.charAt().toUpperCase()+ism.slice(1).toLowerCase()
-console.log(result)
+// let ism = prompt("Ismingizni kiriting: ")
+// let result = ism.charAt().toUpperCase()+ism.slice(1).toLowerCase()
+// console.log(result)
+
+//2-mashq for loopi
+// let names = ['gulshanoy', 'gulbahor', 'gulasal']
+// for(let i=0; i<names.length; i++){
+//     let newNames=names[i].charAt().toUpperCase()+names[i].slice(1).toLowerCase()
+//     console.log(newNames);
+// }
+
+// while loopi
+// let i=0;
+// while (i<8){
+//     console.log(i);
+//     i++
+// }
+
+//do whilw loopi
+let j=0
+do {
+    console.log(j)
+    j++
+}while(j<4)
