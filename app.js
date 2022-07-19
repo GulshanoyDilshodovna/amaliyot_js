7f8ab3a7acacdc1ab188abb5153de88dc9533b13
Gulshanoy <gulshanoydilshodovna@gmail.com>
2022-07-19 15:20:15 +0500

    Add task 2

--- app.js
+++ app.js
@@ -28,8 +28,18 @@
 //Amaliyot 2 19.07.2022
 
 //1-task
-let age = prompt("Yoshingizni kiriting: ")
-let name = prompt("Ismingizni kiriting: ")
-let newName = name.charAt().toUpperCase()+name.slice(1).toLowerCase()
-let result = `Hurmatli ${newName}, siz ${2022-age} - yilda tug'ilgansiz. Siz shu paytgacha ${12*age} oy, ${52*age} hafta, ${365*age} kun, ${365*24*age} soat, ${365*24*60*age} minut va ${365*24*60*60*age} soniya yashadingiz! `
-console.log(result);
+// let age = prompt("Yoshingizni kiriting: ")
+// let name = prompt("Ismingizni kiriting: ")
+// let newName = name.charAt().toUpperCase()+name.slice(1).toLowerCase()
+// let result = `Hurmatli ${newName}, siz ${2022-age} - yilda tug'ilgansiz. Siz shu paytgacha ${12*age} oy, ${52*age} hafta, ${365*age} kun, ${365*24*age} soat, ${365*24*60*age} minut va ${365*24*60*60*age} soniya yashadingiz! ;`
+// console.log(result);
+
+// 2-task
+let names = ["Gulbahor", 'Gulasal', 'Gulshanoy']
+let requested_name = prompt("Biror ism kiriting: ")
+    if(names.includes(requested_name)){
+        console.log(`So'ralgan ism bor`);
+    }else{
+        console.log(`So'ralgan ism yo'q`);
+    } 
+    
