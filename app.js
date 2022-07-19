75a9be2cbd3b6562964b6dd06617f5ac94cf0d57
Gulshanoy <gulshanoydilshodovna@gmail.com>
2022-07-19 14:20:06 +0500

    Add task 1

--- app.js
+++ app.js
@@ -17,8 +17,19 @@
 // }
 
 //do whilw loopi
-let j=0
-do {
-    console.log(j)
-    j++
-}while(j<4)
+// let j=0
+// do {
+//     console.log(j)
+//     j++
+// }while(j<4)
+
+
+
+//Amaliyot 2 19.07.2022
+
+//1-task
+let age = prompt("Yoshingizni kiriting: ")
+let name = prompt("Ismingizni kiriting: ")
+let newName = name.charAt().toUpperCase()+name.slice(1).toLowerCase()
+let result = `Hurmatli ${newName}, siz ${2022-age} - yilda tug'ilgansiz. Siz shu paytgacha ${12*age} oy, ${52*age} hafta, ${365*age} kun, ${365*24*age} soat, ${365*24*60*age} minut va ${365*24*60*60*age} soniya yashadingiz! `
+console.log(result);
