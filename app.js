555dbae55dfb96669ec763d5069d94ce1d2536ca
Gulshanoy <gulshanoydilshodovna@gmail.com>
2022-07-28 23:45:55 +0500

    Add RollDiceGame

--- app.js
+++ app.js
@@ -549,39 +549,113 @@ console.log(getArray);*/
 // player1Btn.addEventListener('click',player1Listener)
 // player2Btn.addEventListener('click',player2Listener)
 
+
+
+
+
 //==========NOW DATE || TIME || VAQT ANIQLAGICH===========//
-function timeShow() {
-  const day = document.getElementById("day"),
-    month = document.getElementById("month"),
-    year = document.getElementById("year"),
-    hours = document.getElementById("hours"),
-    minute = document.getElementById("minute"),
-    second = document.getElementById("second"),
-    now = new Date(),
-    months = [
-      "Jan",
-      "Feb",
-      "Mar",
-      "Apr",
-      "May",
-      "Jun",
-      "Jul",
-      "Aug",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dec",
-    ];
-  day.textContent = now.getDate() < 10 ? "0" + now.getDate() : now.getDate();
-  month.textContent = months[now.getMonth()];
-  year.textContent = now.getFullYear();
-  hours.textContent =
-    now.getHours() < 10 ? "0" + now.getHours() : now.getHours();
-  minute.textContent =
-    now.getMinutes() < 10 ? "0" + now.getMinutes() : now.getMinutes();
-  second.textContent =
-    now.getSeconds() < 10 ? "0" + now.getSeconds() : now.getSeconds();
-}
-setInterval(() => {
-    timeShow()
-}, 1000);
+// function timeShow() {
+//   const day = document.getElementById("day"),
+//     month = document.getElementById("month"),
+//     year = document.getElementById("year"),
+//     hours = document.getElementById("hours"),
+//     minute = document.getElementById("minute"),
+//     second = document.getElementById("second"),
+//     now = new Date(),
+//     months = [
+//       "Jan",
+//       "Feb",
+//       "Mar",
+//       "Apr",
+//       "May",
+//       "Jun",
+//       "Jul",
+//       "Aug",
+//       "Sep",
+//       "Oct",
+//       "Nov",
+//       "Dec",
+//     ];
+//   day.textContent = now.getDate() < 10 ? "0" + now.getDate() : now.getDate();
+//   month.textContent = months[now.getMonth()];
+//   year.textContent = now.getFullYear();
+//   hours.textContent =
+//     now.getHours() < 10 ? "0" + now.getHours() : now.getHours();
+//   minute.textContent =
+//     now.getMinutes() < 10 ? "0" + now.getMinutes() : now.getMinutes();
+//   second.textContent =
+//     now.getSeconds() < 10 ? "0" + now.getSeconds() : now.getSeconds();
+// }
+// setInterval(() => {
+//     timeShow()
+// }, 1000);
+
+
+
+
+
+
+//================ROLL DICE GAME ==============//
+//buttons
+const resetBtn = document.getElementById("reset__btn"),
+  diceBtn = document.getElementById("dice__btn"),
+  holdBtn = document.getElementById("hold__btn"),
+  //dice image
+  diceImg = document.getElementById("img");
+diceImg.style.display = "none";
+//variables
+let currentScore = 0,
+  active = 0,
+  gameOver = true,
+  score = [0, 0];
+
+//switch player function
+const switchPlayer = () => {
+  currentScore = 0;
+  document.getElementById(`account${active}`).textContent = 0;
+  active = active === 0 ? 1 : 0;
+  document.querySelector(".player0").classList.toggle("player__active");
+  document.querySelector(".player1").classList.toggle("player__active");
+};
+//ROLL DICE BUTTON ADDEVENTLISTENER CLICK
+diceBtn.addEventListener("click", () => {
+  if (gameOver) {
+    diceImg.style.display = "block";
+    const randomNbr = Math.floor(Math.random() * 6 + 1);
+    diceImg.src = `./images/dice-${randomNbr}.png`;
+    if (randomNbr !== 1) {
+      currentScore += randomNbr;
+      document.getElementById(`account${active}`).textContent = currentScore;
+    } else {
+      switchPlayer();
+    }
+  }
+});
+//HOLD BUTTON ADDEVENTLISTENER CLICK
+holdBtn.addEventListener("click", () => {
+  if (gameOver) {
+    score[active] += currentScore;
+    document.getElementById(`score${active}`).textContent = score[active];
+    if (score[active] >= 20) {
+        document.querySelector(`.player${active}`).classList.add('winner')
+        gameOver = false;
+    } else {
+      switchPlayer();
+    }
+  }
+});
+//NEW GAME/RESET  BUTTON ADDEVENTLISTENER CLICK
+resetBtn.addEventListener('click', ()=>{
+  currentScore = 0
+  active = 0
+  gameOver = true
+  score = [0, 0]
+  document.getElementById(`account0`).textContent = 0;
+  document.getElementById(`account1`).textContent = 0;
+  document.getElementById(`score0`).textContent = 0;
+  document.getElementById(`score1`).textContent = 0;
+  document.querySelector('.player0').classList.remove('winner')
+  document.querySelector(`.player1`).classList.remove('winner')
+  document.querySelector(`.player0`).classList.add('player__active')
+  document.querySelector(`.player1`).classList.remove('player__active')
+})
