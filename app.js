6706af076452abdbdfdd861a9e5a30581ba31472
Gulshanoy <gulshanoydilshodovna@gmail.com>
2022-08-04 22:55:24 +0500

    Add Color Picker html, css, js file

--- app.js
+++ app.js
@@ -773,91 +773,111 @@ console.log(getArray);*/
 
 //=======MUSIC PLAYER=======//
 //consts - o`zgarmaslar
-const audioContainer = document.getElementById('audio-container'),
-    playBtn  = document.getElementById('play'),
-    prevBtn  = document.getElementById('prev'),
-    nextBtn  = document.getElementById('next'),
-    title  = document.getElementById('title'),
-    audio = document.getElementById('audio'),
-    progressContainer = document.getElementById('progress-container'),
-    progress = document.getElementById('progress'),
-    cover = document.getElementById('cover'),
-    songs = [`Akbarali Ochilov Kelasanmi`,`Mustafo Qirolicham`, `Nodir Zoitov Umrim Seniki`, `Yulduz Usmonova Xasta bo'lma`];
-//let - o`zgaruvchiar
-let songIndex = 0
+// const audioContainer = document.getElementById('audio-container'),
+//     playBtn  = document.getElementById('play'),
+//     prevBtn  = document.getElementById('prev'),
+//     nextBtn  = document.getElementById('next'),
+//     title  = document.getElementById('title'),
+//     audio = document.getElementById('audio'),
+//     progressContainer = document.getElementById('progress-container'),
+//     progress = document.getElementById('progress'),
+//     cover = document.getElementById('cover'),
+//     songs = [`Akbarali Ochilov Kelasanmi`,`Mustafo Qirolicham`, `Nodir Zoitov Umrim Seniki`, `Yulduz Usmonova Xasta bo'lma`];
+// //let - o`zgaruvchiar
+// let songIndex = 0
 
-//functions - funksiyalar
-function loadSong(song){
-    title.innerHTML = song
-    audio.src = `./music/${song}.mp3`
-    cover.src = `./music_img/${song}.jpg`
+// //functions - funksiyalar
+// function loadSong(song){
+//     title.innerHTML = song
+//     audio.src = `./music/${song}.mp3`
+//     cover.src = `./music_img/${song}.jpg`
 
-}
-loadSong(songs[songIndex])
+// }
+// loadSong(songs[songIndex])
 
-//play song function
-function playSong(){
-    audioContainer.classList.add('play')
-    playBtn.querySelector('i.fas').classList.remove('fa-play')
-    playBtn.querySelector('i.fas').classList.add('fa-pause')
+// //play song function
+// function playSong(){
+//     audioContainer.classList.add('play')
+//     playBtn.querySelector('i.fas').classList.remove('fa-play')
+//     playBtn.querySelector('i.fas').classList.add('fa-pause')
 
-    audio.play()
-}
-//pause song function
-function pauseSong(){
-    audioContainer.classList.remove('play')
-    playBtn.querySelector('i.fas').classList.add('fa-play')
-    playBtn.querySelector('i.fas').classList.remove('fa-pause')
+//     audio.play()
+// }
+// //pause song function
+// function pauseSong(){
+//     audioContainer.classList.remove('play')
+//     playBtn.querySelector('i.fas').classList.add('fa-play')
+//     playBtn.querySelector('i.fas').classList.remove('fa-pause')
 
-    audio.pause()
-}
-//prev song function
-function prevSong(){
-    songIndex--
+//     audio.pause()
+// }
+// //prev song function
+// function prevSong(){
+//     songIndex--
     
-    if(songIndex<0){
-        songIndex = songs.length - 1
-    }
-    loadSong(songs[songIndex])
-    playSong()
-}
-//next song function
-function nextSong(){
-    songIndex++
+//     if(songIndex<0){
+//         songIndex = songs.length - 1
+//     }
+//     loadSong(songs[songIndex])
+//     playSong()
+// }
+// //next song function
+// function nextSong(){
+//     songIndex++
     
-    if(songIndex>songs.length-1){
-        songIndex = 0
-    }
-    loadSong(songs[songIndex])
-    playSong()
-}
-// progress update function
-function progressUpdate(e){
-    const {duration, currentTime} = e.srcElement;
-    const progressPercent = (currentTime/duration)*100
-    progress.style.width = `${progressPercent}%`
-}
-// set progress function
-function setProgress(e){
-    const width = this.clientWidth
-    const clickX = e.offsetX
-    const duration = audio.duration
-    audio.currentTime = (clickX / width)*duration
+//     if(songIndex>songs.length-1){
+//         songIndex = 0
+//     }
+//     loadSong(songs[songIndex])
+//     playSong()
+// }
+// // progress update function
+// function progressUpdate(e){
+//     const {duration, currentTime} = e.srcElement;
+//     const progressPercent = (currentTime/duration)*100
+//     progress.style.width = `${progressPercent}%`
+// }
+// // set progress function
+// function setProgress(e){
+//     const width = this.clientWidth
+//     const clickX = e.offsetX
+//     const duration = audio.duration
+//     audio.currentTime = (clickX / width)*duration
+// }
+
+// // events
+// playBtn.addEventListener('click',()=>{
+//     let isPlaying = audioContainer.classList.contains('play')
+//     if(isPlaying){
+//         pauseSong()
+//     }else playSong()
+// })
+// //prev song next song
+// prevBtn.addEventListener('click', prevSong)
+// nextBtn.addEventListener('click', nextSong)
+// //update progress
+// audio.addEventListener('timeupdate', progressUpdate)
+// //
+// progressContainer.addEventListener('click', setProgress)
+// //
+// audio.addEventListener('ended', nextSong)
+
+
+
+
+
+
+//===========COLOR PICKER============//
+const colorFunc = ()=>{
+const colorPicker = document.getElementById('input-color').value
+document.body.style.background = colorPicker
+document.getElementById('input-text').value = colorPicker
 }
 
-// events
-playBtn.addEventListener('click',()=>{
-    let isPlaying = audioContainer.classList.contains('play')
-    if(isPlaying){
-        pauseSong()
-    }else playSong()
-})
-//prev song next song
-prevBtn.addEventListener('click', prevSong)
-nextBtn.addEventListener('click', nextSong)
-//update progress
-audio.addEventListener('timeupdate', progressUpdate)
-//
-progressContainer.addEventListener('click', setProgress)
-//
-audio.addEventListener('ended', nextSong)
+const hexColor = document.getElementById('input-text')
+const demo = document.getElementById('demo')
+const copyFunc = ()=>{
+    navigator.clipboard.writeText(hexColor.value)
+    demo.innerHTML = 'Copied'
+    demo.style.opacity = '1'
+}
